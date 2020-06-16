feba621f7ab9e3fdd54212ca1abf462372938022
hawk-praxs <pratyush.11708251@lpu.in>
2020-06-17 02:07:01 +0530

    Completed the project

---
+++ project/README.md
@@ -0,0 +1,5 @@
+# guessinggame
+Wed Jun 17 02:06:37 IST 2020
+  
+
+23
--- project/guessinggame.sh
+++ project/guessinggame.sh
@@ -4,19 +4,20 @@ function guess(){
     true_ans=$(ls -l |grep "^-"|wc -l)
     while true;
     do
-        echo "Guess no. of files? -> "
+        echo "Guess the no. -> "
         read  number
         if [ $number -lt $true_ans ]
         then
-            echo "Your guess is less then the true number"
+            echo "Your guess is lesser than the true number"
         elif [ $number -gt $true_ans ]
         then
-            echo "Your guess is greater then the true number"
+            echo "Your guess is greater than the true number"
         else
-            echo " Congratulations, you guessed it correctly!"
+            echo " Congratulations, you guessed right!"
         break;
         fi
     done
 }
-echo "Welcome to the Guess Game where you guess the no. of files in the current directory!"
+echo "Welcome to the Guessing Game!"
+echo "Guess the no. of files in the current directory!"
 guess
